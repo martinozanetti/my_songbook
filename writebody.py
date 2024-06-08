506e1a6d39e5da59b318a882a1706e868cb7fe15
Martino <martino.zanetti@gmail.com>
2024-06-08 23:55:14 +0200

    adjust format settings

--- writebody.py
+++ writebody.py
@@ -8,15 +8,19 @@ def create_latex_in_current_directory(output_file):
                 continue
             dir_path = os.path.join(current_directory, dir_name)
             if os.path.isdir(dir_path):
-                tex_file.write(f'\\chapter{{{dir_name}}}\n')
+                # Replace hyphens with spaces in chapter titles
+                chapter_title = dir_name.replace('-', ' ')
+                tex_file.write(f'\\chapter{{{chapter_title}}}\n')
                 for file_name in sorted(os.listdir(dir_path)):
                     file_path = os.path.join(dir_path, file_name)
                     if os.path.isfile(file_path) and file_name.endswith(".txt"):
                         file_base, _ = os.path.splitext(file_name)
-                        tex_file.write(f'\\section{{{file_base}}}\n')
+                        # Replace hyphens with spaces in section titles
+                        section_title = file_base.replace('-', ' ')
+                        tex_file.write(f'\\section{{{section_title}}}\n')
                         relative_file_path = os.path.join(dir_name, file_name).replace("\\", "/")
                         tex_file.write(f'\\VerbatimInput{{{relative_file_path}}}\n')
                 tex_file.write("\n")
 
-# Example usage
+
 create_latex_in_current_directory('body.tex')
